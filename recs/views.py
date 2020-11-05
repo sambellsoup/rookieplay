1c83873c356e7f8c8fe4967562e86f716ec03047
Sam Bell <sambellsoup@gmail.com>
2020-11-05 02:41:21 -0500

    adding new topic function

--- recs/views.py
+++ recs/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Greeting, Document
+from .models import Greeting, Document, Topic
+from .forms import TopicForm
 
 # recommendation packages
 from .text_processing import document_to_text, compile_document_text, text_to_bagofwords, join_and_condense, vectorize_text, recommend_100, format_recommendations, top_100_categories, freq, viz_data, make_viz
@@ -36,3 +38,31 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, "recs/db.html", {"greetings": greetings})
+
+def topics(request):
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'rec/topics.html', context)
+
+def topic(request, topic_id):
+    """Show a single topic and all its entires."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries':entries}
+    return render(request, 'recs/topic.html', context)
+
+def new_topic(request):
+    """Add a new topic"""
+    if request.method != 'POST':
+        # No data submittedl create a blank form.
+        form = TopicForm()
+    else:
+        # POST data submitted; process data.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('recs:topics'))
+
+    context = {'form': form}
+    return render(request, 'recs/new_topic.html', context)
