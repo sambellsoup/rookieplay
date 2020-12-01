c1e44c33c9e49cce819d9dd03ae2ab88f0c1a6a3
Sam <>
2020-12-01 10:42:34 -0500

    changing cache setting from 24 hours to 0 seconds

--- main/settings.py
+++ main/settings.py
@@ -142,7 +142,10 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=0'}
+
+# AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
 # s3 static settings
 AWS_LOCATION = 'static'
