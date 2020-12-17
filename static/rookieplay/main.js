6c6572ffb1c1fd358989887ff5ac2fd23e81ffa1
sambellsoup <sambellsoup@gmail.com>
2020-12-17 05:47:37 -0500

    parsing JSON from API call

--- static/rookieplay/main.js
+++ static/rookieplay/main.js
@@ -41,16 +41,11 @@ function thumbsup(key){
 
   }
   else{
-    console.log("x-value after thumbs up: ", x)
     thumbs_up_list.push(jobs[key]);
     // Use job title to conduct google search for links to live job ads
     var q = jobs[key]
     // window.open('https://serpapi.com/search.json?engine=google_jobs&q=' + q + '+new+york&hl=en')
     document.getElementById("demoa" + x).innerHTML = q
-
-
-    x++
-    console.log("x-value after thumbs up and add: ", x)
 
     // Replace old job title with a new job title to receive judgment from user
     jobs[key] = jobs_bank[0]
@@ -74,6 +69,36 @@ function thumbsup(key){
         console.log("typeof test: ", typeof this.responseText);
         var myObj = JSON.parse(this.responseText);
         document.getElementById("demob" + x).innerHTML = myObj.status;
+        for (a in myObj.job_data){
+          z += "Job Type: " + myObj.job_data[a].job_type + " "
+          z += "Cursor: " + myObj.job_data[a].cursor + " "
+          z += "City: " + myObj.job_data[a].city + " "
+          z += "Salary Offered: " + myObj.job_data[a].salary_offered + " "
+          z += "URL: " + myObj.job_data[a].url + " "
+          z += "Job Description: " + myObj.job_data[a].job_description + " "
+          z += "Job Board: " + myObj.job_data[a].job_board + " "
+          z += "Post Date: " + myObj.job_data[a].post_date + " "
+          z += "Company Name: " + myObj.job_data[a].company_name + " "
+          z += "Category: " + myObj.job_data[a].category + " "
+          z += "Job Title: " + myObj.job_data[a].job_title + " "
+          console.log(z)
+
+          job_type.push(myObj.job_data[a].job_type)
+          cursor.push(myObj.job_data[a].cursor)
+          city.push(myObj.job_data[a].city)
+          salary_offered.push(myObj.job_data[a].salary_offered)
+          url.push(myObj.job_data[a].url)
+          job_description.push(myObj.job_data[a].job_description)
+          job_board.push(myObj.job_data[a].job_board)
+          post_date.push(myObj.job_data[a].post_date)
+          company_name.push(myObj.job_data[a].company_name)
+          category.push(myObj.job_data[a].category)
+          job_title.push(myObj.job_data[a].job_title)
+        }
+
+        x++
+
+        console.log("x-value after thumbs up and add: ", x)
       }
     };
 
