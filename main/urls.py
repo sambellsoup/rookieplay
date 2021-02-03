2d35bd1b5d743093dc7d294bd3097434a7d3456d
Sam Bell <sambellsoup@gmail.com>
2021-02-03 02:58:22 -0500

    candidates urls

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
+from users import views
 
 admin.autodiscover()
 
@@ -20,6 +22,11 @@ urlpatterns = [
     path('', include('recs.urls')),
     path("db/", recs.views.db, name="db"),
     path("admin/", admin.site.urls),
+    path('login/', views.login, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('account/', views.account, name='account'),
+    path('', include('candidates.urls')),
+    path('hiring/', include('recruiters.urls')),
     path('users/', include('users.urls')),
     path('accounts/', include('allauth.urls')),
     path('', include('pwa.urls')),
