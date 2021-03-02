c57418273f656cb6d1c73491f6109bc3744faee3
Sam Bell <sambellsoup@gmail.com>
2021-03-02 16:36:23 -0500

    changing display of job ads

--- static/rookieplay/main.js
+++ static/rookieplay/main.js
@@ -42,7 +42,7 @@ function thumbsup(key){
     var q = jobs[key]
 
     // window.open('https://serpapi.com/search.json?engine=google_jobs&q=' + q + '+new+york&hl=en')
-    document.getElementById("demoa" + x).innerHTML = q
+    // document.getElementById("demoa" + x).innerHTML = q
 
     // Replace old job title with a new job title to receive judgment from user
     jobs[key] = jobs_bank[0]
@@ -80,6 +80,7 @@ function thumbsup(key){
           company_name.push(myObj.job_data[a].company_name)
           category.push(myObj.job_data[a].category)
           job_title.push(myObj.job_data[a].job_title)
+          document.getElementById("demoa" + x).innerHTML = q
           document.getElementById("demob" + x).innerHTML = "Job Title: " + job_title[x];
           document.getElementById("democ" + x).innerHTML = "Location: " + city[x] + "," + state[x];
           document.getElementById("demod" + x).innerHTML = "Posted: " + post_date[x];
@@ -145,8 +146,8 @@ function thumbsup(key){
          {
            "range": {
              "post_date": {
-               "gte": "2021-02-26",
-               "lte": "2021-02-26"
+               "gte": "2021-03-02",
+               "lte": "2021-03-02"
              }
            }
          }
