39b2169770086609a9c3cf3a1c8e2091e03d2f70
Sam Bell <sambellsoup@gmail.com>
2021-05-07 16:52:14 -0400

    adding static assetts

---
+++ company_data_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CompanyDataApiConfig(AppConfig):
+    name = 'company_data_api'
--- main/urls.py
+++ main/urls.py
@@ -36,3 +36,17 @@ urlpatterns = [
 # Only for development purposes.
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+from rest_framework import routers
+from rapidapipractice.api import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Setup automatic URL routing
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
