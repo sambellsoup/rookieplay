56f076fccc35825dea7b9d56190e4e1eb71f7840
Sam Bell <sambellsoup@gmail.com>
2021-05-25 10:31:33 -0400

    adding to models

--- recs/models.py
+++ recs/models.py
@@ -1,9 +1,50 @@
+from __future__ import unicode_literals
 from django.db import models
 import uuid
 from django.contrib.auth.models import User
 from datetime import date
 
+
 # Create your models here.
+
+class Companies(models.Model):
+    company_id = models.BigIntegerField(primary_key=True)
+    slug = models.SlugField()
+    rank = models.IntegerField()
+    name = models.CharField(max_length = 200)
+    revenue = models.IntegerField()
+    city = models.CharField(max_length = 200)
+    state = models.CharField(max_length = 200)
+    underscored_name = models.CharField(max_length = 200)
+    description = models.TextField()
+    urls = models.URLField()
+    adjectives = models.CharField(max_length = 200)
+    logo = models.URLField()
+    image = models.URLField()
+
+
+class Jobs(models.Model):
+    job_id = models.BigIntegerField(primary_key=True)
+    slug = models.SlugField()
+    company_name = models.CharField(max_length = 200)
+    job_title = models.CharField(max_length = 200)
+    job_type = models.CharField(max_length = 200)
+    job_location = models.CharField(max_length = 200)
+    job_link = models.URLField()
+    job_description = models.TextField()
+    company_id = models.ForeignKey('Companies', on_delete=models.CASCADE)
+    pub_date = models.DateField()
+
+    def __str__(self):
+        return "{} on {}".format(
+        self.job_title,
+        self.pub_date.strftime('%Y-%m-%d'))
+
+class Meta:
+    verbose_name = 'job posting'
+    ordering = ['-pub_date']
+
+
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
 
